a66bbd4a327fad6b35f76a4d85bb63fcb9efe2f3
alexaknlss <alem_canalesf@unicah.edu>
2026-08-12 08:43:53 -0600

    Agregar estructura inicial del sistema de gestión de archivos de RRHH

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestorArchivos_RRHH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
