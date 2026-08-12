a7acd54352305c0d8fb9521f7dc567cb92018597
triminiodiego <diego.triminio@unah.hn>
2026-08-12 11:43:02 -0600

    New changes in adding contracts

---
+++ Controllers/ContratoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorArchivos_RRHH.Controllers
+{
+    public class ContratoController : Controller
+    {
+        // GET: Contrato
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
